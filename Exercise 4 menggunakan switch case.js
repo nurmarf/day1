94e6a17104a91d82f5615fc01581498f479b1caa
nurmarf <45351487+nurmarf@users.noreply.github.com>
2019-08-19 22:20:19 +0700

    exercise 4

---
+++ Exercise 4 menggunakan switch case.js
@@ -0,0 +1,61 @@
+var hari = 1;
+var bulan = 5;
+var tanggal = 1945;
+switch (hari && bulan && tanggal) {
+    case hari && bulan && tanggal: {
+        switch (bulan) {
+            case 1: {
+                console.log(bulan = 'januari');
+                break;
+            }
+            case 2: {
+                console.log(bulan = 'februari');
+                break;
+            }
+            case 3: {
+                console.log(bulan = 'maret');
+                break;
+            }
+            case 4: {
+                console.log(bulan = 'april');
+                break;
+            }
+            case 5: {
+                console.log(bulan = 'mei');
+                break;
+            }
+            case 6: {
+                console.log(bulan = 'juni');
+                break;
+            }
+            case 7: {
+                console.log(bulan = 'juli');
+                break;
+            }
+            case 8: {
+                console.log(bulan = 'agustus');
+                break;
+            }
+            case 9: {
+                console.log(bulan = 'september');
+                break;
+            }
+            case 10: {
+                console.log(bulan = 'oktober');
+                break;
+            }
+            case 11: {
+                console.log(bulan = 'november');
+                break;
+            }
+            case 12: {
+                console.log(bulan = 'desember');
+                break;
+            }
+        };
+        console.log(hari, bulan, tanggal);
+        break;
+    }
+    default:
+        (console.log('terima kasih'))
+}
