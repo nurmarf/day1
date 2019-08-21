c1022b5b6d298fe54a376d2a429c5aff5bf633b0
nurmarf <45351487+nurmarf@users.noreply.github.com>
2019-08-21 14:44:27 +0700

    Update 2.js

--- Exercise 7/2.js
+++ Exercise 7/2.js
@@ -1,12 +1,4 @@
-var b = 5;
- var i= 0;
- while(i<=b){
-   var asteriks = "*"
-   var j = 1;
-   while(j<b){
-     asteriks = asteriks + '*'; j++;
-   }
-   console.log(asteriks);
-   i++;
-  }
- 
+var i ="5";
+for(var s = 1; s <=5; s++){
+    console.log('*'. repeat(i));
+ }
