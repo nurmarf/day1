2fc0cde8049b184734d95f46542f35b225f4d990
nurmarf <45351487+nurmarf@users.noreply.github.com>
2019-08-23 18:09:03 +0700

    Update Exercise 3.js

--- day2/Exercise 3.js
+++ day2/Exercise 3.js
@@ -1,29 +1,16 @@
+function dataHandling (text) {
+  for (var i = 0; i < text.length; i++){
+    console.log('Nomor ID: ' + text [i][0]);
+    console.log('Nama Lengkap: ' + text [i][1]);
+    console.log('TTL: ' + text [i][2] + ' ' + text[i][3]);
+    console.log('Hobi: ' + text [i][4]);
+    console.log('');
+  }
+}
 var input = [
-    ["0001", "Roman Alamsyah", "Bandar Lampung", " 21/05/1989", "Membaca"],
-    ["0002", "Dika Sembiring", "Medan", " 10/10/1992", "Bermain Gitar"],
-    ["0003", "Winona", "Ambon", " 25/12/1965", "Memasak"],
-    ["0004", "Bintang Senjaya", "Martapura", " 6/4/1970", "Berkebun"]
-  ]
-
-console.log('Nomor ID: ' + input [0][0]);
-console.log('Nama Lengkap: ' + input [0][1]);
-console.log('TTL: ' + input [0][2] + input [0][3]);
-console.log('Hobi: ' + input [0][4]);
-console.log('');
-
-console.log('Nomor ID: ' + input [1][0]);
-console.log('Nama Lengkap: ' + input [1][1]);
-console.log('TTL: ' + input [1][2] + input [1][3]);
-console.log('Hobi: ' + input [1][4]);
-console.log('');
-
-console.log('Nomor ID: ' + input [2][0]);
-console.log('Nama Lengkap: ' + input [2][1]);
-console.log('TTL: ' + input [2][2] + input [2][3]);
-console.log('Hobi: ' + input [2][4]);
-console.log('');
-
-console.log('Nomor ID: ' + input [3][0]);
-console.log('Nama Lengkap: ' + input [3][1]);
-console.log('TTL: ' + input [3][2] + input [3][3]);
-console.log('Hobi: ' + input [3][4]);
+                ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+                ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+                ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+                ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+            ];
+dataHandling(input);
